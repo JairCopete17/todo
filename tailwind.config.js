b23facf5da90498b2f602a41638c2614d6066d98
Jair <jcopeteroys@gmail.com>
2022-01-11 21:09:59 -0500

    Add tailwind styles

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    fontFamily: {'sans': ['"Source Sans Pro"', 'sans-serif']},
+    extend: {},
+  },
+  plugins: [],
+}
