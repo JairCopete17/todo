818b97a93f1be984e9b3bddb1831314f95e8c3f8
Jair <jcopeteroys@gmail.com>
2022-01-21 21:25:52 -0500

    Add wallet connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,53 @@
 import { useState } from 'react'
+import { Provider } from 'wagmi'
+import { InjectedConnector } from 'wagmi/connectors/injected'
 
 import Form from './components/Form'
 import ListOfTasks from './components/ListOfTasks'
+import Wallet from './components/Wallet'
+
 import uid from './services/getUniqueId'
 
-export default function App () {
-  const data = JSON.parse(localStorage.getItem('data')) || [{ id: uid(), text: 'Start here', state: false }]
+//TODO: Add WalletConnect
+const connectors = () => {
+  return [ new InjectedConnector() ]
+}
 
-  const [tasks, setTasks] = useState(data)
+export default function App () {
+  const localtasks = JSON.parse(localStorage.getItem('localtasks')) || [{ id: uid(), text: 'Start here', state: false }]
+
+  const [tasks, setTasks] = useState(localtasks)
 
   const addTask = (text) => {
     const newTask = { id: uid(), text: text, state: false }
-    localStorage.setItem('data', JSON.stringify([...tasks, newTask]))
+    localStorage.setItem('localtasks', JSON.stringify([...tasks, newTask]))
     setTasks([...tasks, newTask])
   }
 
   const deleteTask = (id) => {
     const remainingTasks = tasks.filter(task => task.id != id)
-    localStorage.setItem('data', JSON.stringify(remainingTasks))
+    localStorage.setItem('localtasks', JSON.stringify(remainingTasks))
     setTasks(remainingTasks)
   }
 
   const completeTask = (task, text, state) => {
     const activeTasks = tasks.filter(e => e.id != task.current.id)
     const completedTask = {id: task.current.id, text:text, state:!state}
-    localStorage.setItem('data', JSON.stringify([...activeTasks, completedTask]))
+    localStorage.setItem('localtasks', JSON.stringify([...activeTasks, completedTask]))
     setTasks([...activeTasks, completedTask])
   }
 
   return (
-    <main className="mx-auto max-w-3xl md:min-h-screen flex flex-col items-center justify-center">
-      <header className="w-11/12 md:w-3/4 flex flex-row items-center gap-4 py-6">
-        <img src="https://emojicdn.elk.sh/👋?style=apple" alt="Waving hand sign emoji" className="w-8 h-8" />
-        <h1 className="text-2xl text-gray-600 font-bold">gm!</h1>
-      </header>
-      <Form addTask={addTask} />
-      <ListOfTasks tasks={tasks} deleteTask={deleteTask} completeTask={completeTask} />
-    </main>
+    <Provider autoConnect connectors={connectors}>
+      <main className="mx-auto max-w-3xl md:min-h-screen flex flex-col items-center justify-center">
+        <header className="w-11/12 md:w-3/4 flex flex-row items-center py-6 select-none">
+          <img src="https://emojicdn.elk.sh/👋?style=apple" alt="Waving hand sign emoji" className="w-8 h-8 mr-4" />
+          <h1 className="text-2xl text-gray-600 font-bold">gm</h1>
+          <Wallet />
+        </header>
+        <Form addTask={addTask} />
+        <ListOfTasks tasks={tasks} deleteTask={deleteTask} completeTask={completeTask} />
+      </main>
+    </Provider>
   )
 }
